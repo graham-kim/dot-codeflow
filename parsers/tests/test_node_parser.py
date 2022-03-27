f56cabf7f7fead194dfd350c6ea4cf0c6849705c
Graham Kim <graham.kim@cantab.net>
2022-03-28 00:31:52 +0100

    Issue #2: Unhappy path unit tests for NodeParser

--- parsers/tests/test_node_parser.py
+++ parsers/tests/test_node_parser.py
@@ -32,3 +32,21 @@ class TestNodeParser(unittest.TestCase):
         class1 = self.parser.finished_classes[1]
         self.assertEqual(3, len(class1.member_variables), msg= \
             "Second class should have expected number of member variables")
+
+    def test_bad_param_line(self) -> None:
+        with self.assertRaises(Exception) as ex:
+            self.parse_test_input()
+        self.assertTrue("Expected a function to parse this line under" in str(ex.exception), \
+            msg=f"Exception message does not explain problem:\n  {ex.exception}")
+
+    def test_method_without_class(self) -> None:
+        with self.assertRaises(Exception) as ex:
+            self.parse_test_input()
+        self.assertTrue("Expected a class to parse this line under" in str(ex.exception), \
+            msg=f"Exception message does not explain problem:\n  {ex.exception}")
+
+    def test_class_broken_by_comment(self) -> None:
+        with self.assertRaises(Exception) as ex:
+            self.parse_test_input()
+        self.assertTrue("Expected a class to parse this line under" in str(ex.exception), \
+            msg=f"Exception message does not explain problem:\n  {ex.exception}")
