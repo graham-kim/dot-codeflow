29d2b855570f2f1ced584bf64a2196e7015101f0
Graham Kim <graham.kim@cantab.net>
2022-08-28 22:44:01 +0100

    Set some global attributes manually

--- flow.py
+++ flow.py
@@ -8,6 +8,7 @@ def setup_parser() -> argparse.ArgumentParser:
     parser.add_argument('filenames', nargs='+', help= "Input filenames")
     parser.add_argument('--gen-inputs', action='store_true', help= \
         "Generate input file content for given filenames")
+    parser.add_argument('--rankdir', default="TD", help="Direction to draw graph")
     return parser
 
 def generate_input_files(filenames: tp.List[str]):
@@ -25,16 +26,23 @@ def parse_files(filenames: tp.List[str]):
 
     return parser.get_pydot_dot()
 
-def pretty_print_dot(dot):
+def pretty_print_with_global_attr(dot, args):
     dot.write("temp.dot")
 
     indent_level = 0
     with open("temp.dot", "r") as inF:
-        for line in inF:
+        for i,line in enumerate(inF):
             if line.strip().endswith("}"):
                 indent_level -= 1
 
-            print(" "*indent_level*4 + line)
+            if line.strip().startswith("subgraph"):
+                print("")
+
+            print(" "*indent_level*4 + line.strip('\n').strip('\r'))
+
+            if i == 0:
+                print(f'    rankdir={args.rankdir}')
+                print('    node [shape="box" style="filled" fillcolor="white"]')
 
             if line.strip().endswith("{"):
                 indent_level += 1
@@ -45,4 +53,4 @@ if __name__ == '__main__':
         generate_input_files(args.filenames)
     else:
         dot = parse_files(args.filenames)
-        pretty_print_dot(dot)
+        pretty_print_with_global_attr(dot, args)
